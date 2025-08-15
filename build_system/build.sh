c41d703fcb950262ff688f34ea22e80966e43ede
Dmitriy Krasnyansky <k.dimonych@gmail.com>
2025-08-15 18:05:45 +0300

    fix: first implementation

---
+++ build_system/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+PARAM=""
+
+# Retrieve the target from the current filename, if no target specified,
+# the variable will be empty
+TARGET=$(echo $0 | cut -s -f2 -d- | cut -s -f1 -d.)
+if [[ -n $TARGET ]]
+then
+    # Target is not null, specify the build parameters
+    echo "Building for target: $TARGET"
+    PARAM="-DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-${TARGET}.cmake -DTARGET=${TARGET} -GNinja"
+fi
+
+rm -rf build && mkdir build && cd build
+cmake .. $PARAM
+cmake --build .
